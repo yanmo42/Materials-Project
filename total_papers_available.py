ab6b5e75043acbe4d56537c4300e7d4429687624
ian <ianmoog42@gmail.com>
2025-08-04 20:20:15 -0400

    trimmed repo for clarity

--- total_papers_available.py
+++
@@ -1,9 +0,0 @@
-import requests; print(requests.get('https://api.openalex.org/works?search=ferroelectric&filter=open_access.is_oa:true&per-page=1').json()['meta']['count'])
-
-
-print(requests.get('https://api.openalex.org/works?search=ferromagnet&filter=open_access.is_oa:true&per-page=1').json()['meta']['count'])
-
-# 60496 on 7/21/25
-# 176482 for ferromagnet/magnetic
-
-
